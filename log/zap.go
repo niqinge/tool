b74e0b631e582b8ed80a56a26af405482a039bef
novice <huangzhenqin@mintechai.com>
2020-04-08 16:33:50 +0800

    fix log

--- log/zap.go
+++ log/zap.go
@@ -53,18 +53,3 @@ func getLogWriter(project string) zapcore.WriteSyncer {
 	return consolSyncers
 }
 
-func Warn(msg string, fields ...zap.Field) {
-	Logger.Warn(msg, fields...)
-}
-
-func Error(msg string, fields ...zap.Field) {
-	Logger.Error(msg, fields...)
-}
-
-func Info(msg string, fields ...zap.Field) {
-	Logger.Info(msg, fields...)
-}
-
-func Debug(msg string, fields ...zap.Field) {
-	Logger.Debug(msg, fields...)
-}
